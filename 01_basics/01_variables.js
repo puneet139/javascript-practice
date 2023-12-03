a4c97c75c310d33f299c54070252b69306a63fea
Puneet Chaturvedi <38996759+puneet139@users.noreply.github.com>
2023-12-03 13:54:05 +0000

    basics of js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 123
+let accountEmail = "test123@google.com"
+var accountName = "test-name"
+accountCity = "Bengaluru"
+
+// accountId = 231
+//let is prefered over var for declaring variables because of scope issues, accountCity naming convention is not recommended
+console.log(accountId);
+console.table([accountEmail, accountName, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+// below are valid datatypes
+//number, bigint
+//string
+//boolean
+//null
+//object
+//undefined
+
+let age = 18
+let name = "test"
+let check = false
+
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(typeof age);
+
+console.log(typeof name);
+
+console.log(typeof check);
